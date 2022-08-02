b423b77c85fc167ddf0545d98410d538ec0e1650
gitarun786 <63334712+gitarun786@users.noreply.github.com>
2022-08-02 11:19:04 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo $2 > meesage.txt
-curl $1 -x post -H 'Content-type: application/json' --data "message.txt"
+data='{"text":"'${2}'"}'
+echo $data > message.txt
+curl $1 -X post -H 'Content-type: application/json' --data "@message.txt"
